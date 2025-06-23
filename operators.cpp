fddb1277c4ca7afcf9d8576edea75cb0f25bd831
Chrispin Obudho <50041864+krizah19@users.noreply.github.com>
2025-06-23 16:16:18 +0100

    operator
    commit

--- operators.cpp
+++ operators.cpp
@@ -4,10 +4,11 @@ using namespace std;
 
 int main()
 {
-    int age = 35;
-    bool isEligible = age > 18;
+    int age = 20;
+    int salary = 50000;
+    bool isEligible = (age > 18 && age < 65) && (salary > 30000);
 
-    cout << boolalpha << isEligible;
+    cout << boolalpha << !isEligible;
 
     return 0;
 }
